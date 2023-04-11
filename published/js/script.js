0e446de72f104c184d44c3038f312c6764269466
jarekgos <61649244+jarekgos@users.noreply.github.com>
2023-04-11 19:52:28 +0200

    Batch update

---
+++ published/js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-gpr-knur-w","displayName":"GPR Knurów","link":{"linkType":"LinkTypePage","href":"#!page-gpr-knur-w"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
